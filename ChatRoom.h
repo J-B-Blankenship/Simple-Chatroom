ff9aa6acd4af46a871ef032d2d4749140b7c63b2
J-B-Blankenship <jblankenship9824@gmail.com>
2022-08-18 09:18:34 -0500

    Resolved a few errors of the base functionality. Completed the first task.

--- ChatRoom.h
+++ ChatRoom.h
@@ -14,6 +14,31 @@ namespace Renci
         std::vector<std::string> history{};
         std::mutex m_mutex{};
 
+        void checkForCommands(const std::string& message)
+        {
+            for(uint64_t i = 0; i < message.size(); i++)
+            {
+                //Check for empty space before a possible command
+                if(message[i] == ' ')
+                {
+                    continue;
+                }
+                else if(message[i] != '!')
+                {
+                    return; //Extra characters before command is invalid
+                }
+                //Start of a command
+                else if(message[i] == '!')
+                {
+                    if(message.find("echo ", i) != std::string::npos)
+                    {
+                        history.push_back(message.substr(i + 6)); //6 comes from ! plus 'echo' plus the space
+                        return; //Prevent further checking of commands
+                    }
+                }
+            }
+        }
+
     public:
         grpc::Status sendMessage(grpc::ServerContext* contex,
             const Post* post,
@@ -22,13 +47,15 @@ namespace Renci
             //Add some thread safety
             const std::lock_guard<std::mutex> lock{ m_mutex };
 
-            //Send back all updates to chat room that have not been received
-            std::vector<std::string>::const_iterator iter = history.begin();
-            iter += post->identification();
-            response->mutable_messages()->Add(iter, history.cend());
+            for(uint64_t i = post->identification(); i < history.size(); i++)
+            {
+                std::string* temp = response->add_messages();
+                *temp = history[i];
+            }
 
             //Add the new message
             history.push_back(post->message());
+            checkForCommands(post->message());
             return grpc::Status::OK;
         }
 
@@ -36,10 +63,14 @@ namespace Renci
             const Post* post,
             Response* response) override
         {
+            //Add some thread safety
             const std::lock_guard<std::mutex> lock{ m_mutex };
-            std::vector<std::string>::const_iterator iter = history.begin();
-            iter += post->identification();
-            response->mutable_messages()->Add(iter, history.cend());
+            
+            for(uint64_t i = post->identification(); i < history.size(); i++)
+            {
+                std::string* temp = response->add_messages();
+                *temp = history[i];
+            }
             return grpc::Status::OK;
         }
     };
